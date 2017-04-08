95f8113fed795fe136c1d4795c60babf365b7c67
Jeremy Grondahl <jeremygrondahl@gmail.com>
2017-04-08 15:49:31 -0500

    fixed untracked files

--- src/app/hero.image.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var HeroImage = (function () {
-    function HeroImage() {
-    }
-    return HeroImage;
-}());
-exports.HeroImage = HeroImage;
-//# sourceMappingURL=hero.image.js.map
